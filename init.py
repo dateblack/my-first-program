a14ec5484bc84ef7b12c1a9ec5e326d0a701630c
jaun camilo <juank-1922-32055@hotmail.com>
2023-01-18 20:37:56 -0500

    he ingresado 2 nuevos proyectos

---
+++ init.py
@@ -0,0 +1,31 @@
+def divisors(num):
+    divisors = []
+    for x in range(1, num+1):
+        if num%x == 0:
+            divisors.append(x)
+
+    return divisors
+
+def main():
+
+    try:
+        num = int(input('Ingresa un número entero positivo: '))
+        if num <0 or num%1 != 0:
+            raise Exception('Debes ingresar un número entero positivo 🙄')
+        print(divisors(num))
+
+    except ValueError:
+        print('Debes ingresar un número 👀')
+
+    except Exception as ve:
+        print(ve)
+        exit()
+
+    else:
+        print('Ningun error en el camino 😁')
+
+    finally:
+        print('Terminó mi programa 💓')
+
+if __name__=='__main__':
+    main()
---
+++ pop.py
@@ -0,0 +1,37 @@
+class traingulos():
+    def __init__(self):
+        self.lado1=int(input("ingrese el valor del  primer lado"))
+        self.lado2=int(input("ingrese el calor de segundo lado"))
+        self.lado3=int(input("ingrese el valor del tercer lado"))
+        
+    def mostrar(self):
+        print(" Lado:" self.lado1)
+        print(" Lado 2:" self.lado2)
+        print(" Lado 3 :" self.lado3)
+ 
+class equilateros(traingulos):
+    def __init__(self):
+        super().__init__()
+        if self.lado1==self.lado2 and self.lado1==self.lado3:
+            print("el triangulo es equilatero")
+        else :
+            print("el traingulo no es equilatero")
+    def Mostrar(self):
+        super().mostrar
+
+class escaleno(traingulos):
+    def __init__(self):
+        super().__init__()
+        if self.lado1!=self.lado2 and self.lado1!=self.lado3:
+            print("el triangulo es escaleno")
+        else:
+            print("el traingulo es isoceles")   
+    def show(self):
+        super().mostrar
+#bloque principal
+Figura=traingulos()
+#Figura.mostrar()
+Equilatero=equilateros()
+Equilatero.Mostrar()
+Escaleno=escaleno()
+Escaleno.show()
